4ba1fc04092d77ee73986c1483938613dc92990f
Pablo Morales <pablo@espe.edu.ec>
2026-04-09 10:30:36 -0500

    versión1  rama1

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -8,8 +8,9 @@ int num1,num2;
 float mu, re;
 cout<<"Ingrese valor de a: "; cin >> n1;
 cout<<"Ingrese valor de b: "; cin >> n2;
-mu = num1 *n2;
-re = num1 - n2
+mu = num1 * num2;
+re = num1 - num2;
+su = num1 + num2;
 
 cout <<"El total de la multiplicación es: " << s;
 cout <<"El total de la división es: " << s;
