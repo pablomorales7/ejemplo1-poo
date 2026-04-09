fb41358e302101fdc7884b6adfa4fe651b1fb411
Pablo Morales <pablo@espe.edu.ec>
2026-04-09 09:22:58 -0500

    Version 1

---
+++ ejemplo1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+main(){
+    // Hola Mundo
+int num1,num2;
+float mu, re;
+cout<<"Ingrese valor de a: "; cin >> n1;
+cout<<"Ingrese valor de b: "; cin >> n2;
+mu = num1 *n2;
+re = num1 - n2
+
+cout <<"El total de la multiplicación es: " << s;
+cout <<"El total de la división es: " << s;
+
+}
